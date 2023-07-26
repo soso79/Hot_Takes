97507ae0b4465ef50607f0136e63b99ec16d025f
sofiane bahri <bahrisofiane79@gmail.com>
2023-07-26 10:32:16 +0200

    creation route get

--- fullstack/backend/app.js
+++ fullstack/backend/app.js
@@ -2,6 +2,15 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+
+
 app.use('/api/Hot_Takes', (req, res, next) => {
     const Hot_Takes = [
       {
