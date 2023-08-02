2b65d02d1ff14e179882b62c1ca72d59d6ac5880
sofiane bahri <bahrisofiane79@gmail.com>
2023-08-02 14:58:53 +0200

    schéma sauce mongoose

--- fullstack/backend/models/Sauce.js
+++ fullstack/backend/models/Sauce.js
@@ -1,11 +1,18 @@
-const mongoose = require('mongoose');
+// créer un schéma de données
+const mongoose = require("mongoose");
 
 const sauceSchema = mongoose.Schema({
-  title: { type: String, required: true },
-  description: { type: String, required: true },
-  imageUrl: { type: String, required: true },
-  userId: { type: String, required: true },
-  price: { type: Number, required: true },
+  userId: { type: String, required: [true, "userId required"] },
+  name: { type: String, required: [true, "name required"] },
+  manufacturer: { type: String, required: [true, "manufacturer required"] },
+  description: { type: String, required: [true, "description required"] },
+  mainPepper: { type: String, required: [true, "mainPepper required"] },
+  imageUrl: { type: String, required: [true, "imageUrl required"] },
+  heat: { type: Number, required: [true, "heat required"] },
+  likes: { type: Number, required: [true, "likes required"] },
+  dislikes: { type: Number, required: [true, "dislikes required"] },
+  usersLiked: { type: Array, required: [true, "usersLiked required"] },
+  usersDisliked: { type: Array, required: [true, "usersDisliked required"] },
 });
 
-module.exports = mongoose.model('Sauce', sauceSchema);
+module.exports = mongoose.model("Sauce", sauceSchema);
