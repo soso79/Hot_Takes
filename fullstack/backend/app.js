1b860123be80b6cb839cf3784e44b5bc9285f025
sofiane bahri <bahrisofiane79@gmail.com>
2023-08-03 16:37:01 +0200

    install all package

--- fullstack/backend/app.js
+++ fullstack/backend/app.js
@@ -23,7 +23,11 @@ const mongoose = require("mongoose");
 
 // connection au cluster mongoDB
 mongoose
-  .connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true })
+/*mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true*/
+
+
+
+  .connect(`mongodb+srv://soso:Manimal@cluster0.r62xbdt.mongodb.net/?retryWrites=true&w=majority`)
   .then(() => console.log("Connexion à la base de données réussie !"))
   .catch(() => console.log("Connexion à la base de données échouée !"));
 
